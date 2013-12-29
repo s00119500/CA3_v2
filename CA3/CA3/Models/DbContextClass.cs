c670d1c5861837a1de8f04bc34e621142381d799
Thomas <s00119500@mail.itsligo.ie>
2013-12-29 15:59:03 +0000

    Db Context for Home page
    Created Db Context Class for entity framework/ models folder. Created
    Products class.

---
+++ CA3/CA3/Models/DbContextClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CA3.Models
+{
+    public class DbContextClass : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+    }
+}
